ffd98a040ba16215d86caf7a86d52cb9cb485c6a
Adam Wandoch <wandoch.adam@gmail.com>
2022-05-29 06:26:15 +0100

    Initial commit

---
+++ src/main/java/com/adamwandoch/adamstudiesapis/AdamStudiesApisApplication.java
@@ -0,0 +1,13 @@
+package com.adamwandoch.adamstudiesapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdamStudiesApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdamStudiesApisApplication.class, args);
+	}
+
+}
